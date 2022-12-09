3635697c33cd3b48a936b328ac10c69360895a40
Michaela Meissner <m.p.meissner85@gmail.com>
2022-12-08 21:01:55 -0500

    Beginning of classes

---
+++ dinosaur.py
@@ -0,0 +1,7 @@
+class Dinosaur:
+    def __init__(self, name, attack_power, health) -> None:
+        self.name = ""
+        self.attack_power = 10
+        self.health = 100 
+        
+
---
+++ robot.py
@@ -0,0 +1,8 @@
+class Robot:
+    def __init__(self, name, health, active_weapon) -> None:
+        self.name = "BeeBoop"
+        self.health = 100
+        self.active_weapon = "Battle Axe"
+
+
+
