d2142675d2f105eba5348d223b96ff519a4581c1
Michaela Meissner <m.p.meissner85@gmail.com>
2022-12-11 20:33:12 -0500

    I still can't figure out how to make the while loop in the battlefield stop at zero health for one class

--- battlefield.py
+++ battlefield.py
@@ -12,30 +12,46 @@ class Battlefield:
         pass
 
     def run_game(self):
+        print("")
         print("Let's get ready to rumble!") 
 
     def display_welcome(self):
-        print("Welcome to Battlefield! Tonight we have the ultimate face off of the chilling circuit-board, Bee Boop, versus the terrifying stegosarus, Thunder Dome!")
-
+        print("")
+        print("Welcome to Battlefield Royale! Tonight we have the ultimate face off for your viewing pleasure. The chilling circuit-board, Bee Boop, versus the terrifying stegosarus, Thunder Dome!")
+        print("")
 
     def battle_phase(self):
         print("Let's begin!")  
         print("")
-        while Robot.health != 0:
-            print(Dinosaur.name, "just attacked with", Dinosaur.attack_power, "damage to", Robot.name,"!")
-            Robot.health = Robot.health - Dinosaur.attack_power
+        while Robot.health != 0 or Dinosaur.health != 0:  #I got the while loop to repeat until health = 0, but I couldn't make it stop when just one class reached 0 health. 
+            Dinosaur.attack = print(Dinosaur.name, "just attacked with", Dinosaur.attack_power, "damage to", Robot.name,"!")
+            Robot.health = Robot.health - Dinosaur.attack_power 
             print(Robot.name, "has", Robot.health, "health remaining.")
             print("")
-            print(Robot.name, "attacks", Dinosaur.name, "with", Weapon.name, "for", Weapon.attack_power, "damage.")
+            Robot.attack = print(Robot.name, "attacks", Dinosaur.name, "with", Weapon.name, "for", Weapon.attack_power, "damage.")
             Dinosaur.health = Dinosaur.health - Weapon.attack_power 
             print(Dinosaur.name, "has", Dinosaur.health, "health remaining.")
             print("")
-                   
+
 
     def display_winner(self):
         print("Ding ding ding!! We have a new Champion! Congratulations Thunder Dome!")
         print("")
 
+
+##I had originially tried to make it a while loop, but I couldn't get it stop when just 1 health was 0.
+    # def battle_phase(self):
+    #     print("Let's begin!")  
+    #     print("")
+    #     while Robot.health > 0: 
+    #         print(Dinosaur.name, "just attacked with", Dinosaur.attack_power, "damage to", Robot.name,"!")
+    #         Robot.health = Robot.health - Dinosaur.attack_power
+    #         print(Robot.name, "has", Robot.health, "health remaining.")
+    #         print("")
+    #         print(Robot.name, "attacks", Dinosaur.name, "with", Weapon.name, "for", Weapon.attack_power, "damage.")
+    #         Dinosaur.health = Dinosaur.health - Weapon.attack_power 
+    #         print(Dinosaur.name, "has", Dinosaur.health, "health remaining.")
+    #         print("")
 
 
 
--- robot.py
+++ robot.py
@@ -16,7 +16,8 @@ class Robot:
     def attack(self, Dinosaur): 
         Robot.active_weapon = Weapon(15) - Dinosaur.health
         print("Bee Boop has used", Robot.active_weapon.name, "to land", Weapon.attack_power, "damage to Thunder Dome!")
-        
+
+
 Robot.name = "Bee Boop"
 Robot.health = 100
 Robot.active_weapon = Weapon 
